32d0eeb974ecd5ccde574828166c0c2b35b8a722
Ladesic, Charles <charles.ladesic@verizonwireless.com>
2019-02-27 17:18:52 -0600

    jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,8 +6,9 @@ var minComNum = 19;
 var maxComNum = 120;
 var userMinNum = 1;
 var userMaxNum = 12;
-var randomUser = 0;
+var randomUser = "";
 var randomAssign = [];
+
 
 $(document).ready(function () {
 
@@ -16,19 +17,22 @@ $(document).ready(function () {
         randomCom = Math.floor(Math.random() * (+maxComNum - +minComNum) + +minComNum);
         console.log("Random Number Generated : " + randomCom);
 
-        for (var i = 0; i < userChoice.length; i++) {
+        //for (var i = 0; i < userChoice.length; i++) {
+        //    randomAssign = Math.floor(Math.random() * (+userMaxNum - +userMinNum) + +userMinNum);
+        //    console.log("Random User Number Generated : " + randomAssign);
+            
+           
+        
+            
+            
+       // }
+      
+        //console.log("Random number assigned to randomAssign outside loop:  " + randomUser);
+
+        
+        $("#steelAxeBtn").on("click", function() {
             randomUser = Math.floor(Math.random() * (+userMaxNum - +userMinNum) + +userMinNum);
             console.log("Random User Number Generated : " + randomUser);
-            randomAssign = randomUser;
-            console.log("Random number assigned to randomAssign:  " + randomAssign);
-
-            
-        }
-        $("#steelAxeBtn").on("click", function() {
-          
-          
-            //randomAssign.attr("#steelAxeBtn", randomUser[i])
-            console.log("Button pushed the value: " + $("#steelAxeBtn"));
 
         });
 
