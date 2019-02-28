d937d9a057934f52bafc2562b5075f3ab4508bd9
Ladesic, Charles <charles.ladesic@verizonwireless.com>
2019-02-28 14:06:42 -0600

    working btns

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,128 @@
 
-var userChoice = ["axe", "sword", "bow", "warhammer"];
-
 var randomCom = 0;
 var minComNum = 19;
 var maxComNum = 120;
 var userMinNum = 1;
 var userMaxNum = 12;
-var randomUser = "";
-var randomAssign = [];
+var totalHit = 0;
+var win = 0;
+var lose = 0;
 
 
 
 $(document).ready(function () {
 
-    $("#steelAxeBtn").attr(Math.floor(Math.random() * (+userMaxNum - +userMinNum) + +userMinNum));
-    console.log("Here is the information after attr" + S("#steelAxeBtn"));
-    
+  function start() {
+     $("#hitScore").empty()
+    btn1 = Math.floor(Math.random() * (+userMaxNum - +userMinNum) + +userMinNum);
+    console.log("Random User Number Generated BTN1 : " + btn1);
 
-    function start() {
+    btn2 = Math.floor(Math.random() * (+userMaxNum - +userMinNum) + +userMinNum);
+    console.log("Random User Number Generated BTN2 : " + btn2);
 
-        randomCom = Math.floor(Math.random() * (+maxComNum - +minComNum) + +minComNum);
-        console.log("Random Number Generated : " + randomCom);
+    btn3 = Math.floor(Math.random() * (+userMaxNum - +userMinNum) + +userMinNum);
+    console.log("Random User Number Generated BTN3: " + btn3);
 
-      //  for (var i = 0; i < userChoice.length; i++) {
-      //     randomAssign = Math.floor(Math.random() * (+userMaxNum - +userMinNum) + +userMinNum);
-      //      console.log("Random User Number Generated : " + randomAssign);
-            
-           
-        
-            
-            
-       // }
-      
-        //console.log("Random number assigned to randomAssign outside loop:  " + randomUser);
+    btn4 = Math.floor(Math.random() * (+userMaxNum - +userMinNum) + +userMinNum);
+    console.log("Random User Number Generated BTN4: " + btn4);
 
-        
-      //  $("#steelAxeBtn").on("click", function() {
-       //     randomUser = Math.floor(Math.random() * (+userMaxNum - +userMinNum) + +userMinNum);
-       //     console.log("Random User Number Generated : " + randomUser);
+    //Begining Score "Health Score"       
+    randomCom = Math.floor(Math.random() * (+maxComNum - +minComNum) + +minComNum);
+    console.log("Random Number Generated : " + randomCom);
+    $("#healthNumber").val(randomCom);
 
-       // });
+    //Button 1 "Axe"         
+    $("#steelAxeBtn").click(function () {
+      $("#hitScore").val(btn1);
+      if (randomCom !== totalHit || randomCom > totalHit) {
+        randomCom = randomCom - btn1;
+        $("#healthNumber").val(randomCom);
+     
+      } if (randomCom < totalHit) {
+        alert("You lose");
+        lose++;
+        $(".badge-error").text(lose);
+        $("#hitScore").empty();
+        start();
+      }
+     
+      if (randomCom === totalHit) {
+        alert("You Win")
+        win++;
+        $(".badge-success").text(win);
+        $("#hitScore").empty();
+        start();
+      } else $("#healthNumber").val(randomCom);
+    });
 
-    }
-    start();
+    //Button 2 "Sword"  
+    $("#swordBtn").click(function () {
+      $("#hitScore").val(btn2);
+      if (randomCom !== totalHit || randomCom > totalHit) {
+        randomCom = randomCom - btn2;
+        $("#healthNumber").val(randomCom);
+      } if (randomCom < totalHit) {
+        alert("You lose");
+        lose++;
+        $(".badge-error").text(lose);
+        $("#hitScore").empty();
+        start();
+      }
+      if (randomCom === totalHit) {
+        alert("You Win")
+        win++;
+        $(".badge-success").text(win);
+        $("#hitScore").empty();
+        start();
+      }
+      else $("#healthNumber").val(randomCom);
+    });
+
+    //Button 3 "Bow" 
+    $("#bowBtn").click(function () {
+      $("#hitScore").val(btn3);
+      if (randomCom !== totalHit || randomCom > totalHit) {
+        randomCom = randomCom - btn3;
+        $("#healthNumber").val(randomCom);
+      } if (randomCom < totalHit) {
+        alert("You lose");
+        lose++;
+        $(".badge-error").text(lose);
+        $("#hitScore").empty();
+      }
+      if (randomCom === totalHit) {
+        alert("You Win")
+        win++;
+        $(".badge-success").text(win);
+        $("#hitScore").empty();
+        start();
+
+      } else $("#healthNumber").val(randomCom);
+    });
+
+    //Button 4 "Warhammer" 
+    $("#warhammerBtn").click(function () {
+      $("#hitScore").val(btn4);
+
+      if (randomCom !== totalHit || randomCom > totalHit) {
+        randomCom = randomCom - btn4;
+        $("#healthNumber").val(randomCom);
+      } if (randomCom < totalHit) {
+        alert("You lose");
+        lose++;
+        $(".badge-error").text(lose);
+        $("#hitScore").empty()
+      }
+      if (randomCom === totalHit) {
+        alert("You Win")
+        win++;
+        $(".badge-success").text(win);
+        $("#hitScore").empty()
+        start();
+      } else $("#healthNumber").val(randomCom);
+    });
+
+  }
+  start();
+
 });
